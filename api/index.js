810574a44b4423cdf6d9195b3979bd761d20cbe6
Rabin Kumar Baniya <rabinkrbaniya09@.com>
2024-02-14 14:25:59 +0545

    connected to the database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv")
 
-app.get("/", (req,res) => {
-    res.send("Social Media Rest Api!!!")
-})
+dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('MongoBD is connected');
+}).catch((err) => {
+    console.log(err);
+});
 
 app.listen(3000, () => {
     console.log("App is running");
-})
+});
