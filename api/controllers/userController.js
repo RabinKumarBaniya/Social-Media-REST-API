1e22b7d781bf2a5f321fb064b08b53ccf3cefd9d
Rabin Kumar Baniya <rabinkrbaniya09@.com>
2024-02-20 21:08:11 +0545

    Added route functionality for follow and unfollow operations

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -32,4 +32,68 @@ const updateUserController = async(req, res, next) =>{
     }
 }
 
-module.exports = {getUserController, updateUserController}
+
+const followUserController = async(req,res,next) => {
+    const {userId} = req.params
+    const {_id} = req.body
+    
+    try {
+        if(userId === _id) {
+            throw new CustomError("You cannot follow yourself", 500)
+        }
+        const userToFollow = await User.findById(userId)
+        const loggedInUser = await User.findById(_id)
+
+        if(!userToFollow || !loggedInUser){
+            throw new CustomError("user not found", 404)
+        }
+        if(loggedInUser.following.includes(userId)){
+            throw new CustomError("Already following this user", 400)
+        }
+        loggedInUser.following.push(userId)
+        userToFollow.followers.push(_id)
+
+        await loggedInUser.save()
+        await userToFollow.save()
+
+        res.status(200).json({message: "Successfully followed user"})
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+
+const unfollowUserController = async(req,res,next) => {
+    const {userId} = req.params
+    const {_id} = req.body
+    
+    try {
+        if(userId === _id) {
+            throw new CustomError("You cannot unfollow yourself", 500)
+        }
+        const userToUnfollow = await User.findById(userId)
+        const loggedInUser = await User.findById(_id)
+
+        if(!userToUnfollow || !loggedInUser){
+            throw new CustomError("user not found", 404)
+        }
+        if(!loggedInUser.following.includes(userId)){
+            throw new CustomError("Not following this user", 400)
+        }
+
+        loggedInUser.following = loggedInUser.following.filter(id=>id.toString() !== userId)
+        userToUnfollow.followers=userToUnfollow.followers.filter(id=>id.toString() !== _id)
+
+        await loggedInUser.save()
+        await userToUnfollow.save()
+
+        res.status(200).json({message: "Successfully unfollowed user"})
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+
+module.exports = {getUserController, updateUserController,followUserController, unfollowUserController}
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,13 @@
 const express = require("express")
-const { getUserController, updateUserController } = require("../controllers/userController")
+const { getUserController, updateUserController, followUserController, unfollowUserController } = require("../controllers/userController")
 const router = express.Router()
 
 router.get("/:userId", getUserController)
 
 router.put("/update/:userId",updateUserController)
+
+router.post("/follow/:userId", followUserController)
+
+router.post("/unfollow/:userId", unfollowUserController)
 
 module.exports = router
