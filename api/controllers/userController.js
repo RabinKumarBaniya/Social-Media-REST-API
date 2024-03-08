9a694b9e69b2889a58d51c71f906a6f8cac9f0d1
Rabin Kumar Baniya <rabinkrbaniya09@.com>
2024-03-08 09:51:12 +0545

    Implemented block, unblock, and blocklist features in backend

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -94,6 +94,81 @@ const unfollowUserController = async(req,res,next) => {
     }
 }
 
+const blockUserController = async (req, res, next) => {
+    const {userId} = req.params;
+    const {_id} = req.body
+
+    try {
+        if(userId === _id){
+            throw new CustomError("You can not block yourself", 500)
+        }
+        const userToBlock = await User.findById(userId)
+        const loggedInUser = await User.findById(_id)
+
+        if(!userToBlock || !loggedInUser) {
+            throw new CustomError("User not found!", 404)
+        }
+        if(loggedInUser.blocklist.includes(userId)){
+            throw new CustomError("This user is already blocked!", 404)
+        }
+
+        loggedInUser.blocklist.push(userId)
+        loggedInUser.following= loggedInUser.following.filter(id => id.toString()!==userId)
+        userToBlock.followers = userToBlock.followers.filter(id=>id.toString()!==_id)
+
+        await loggedInUser.save()
+        await userToBlock.save()
+
+        res.status(200).json({message: "Successfully blocked user!"})
+    } catch (error) {
+        
+    }
+}
+
+const unblockUserController = async(req,res, next) => {
+    const {userId} = req.params;
+    const {_id} = req.body
+
+    try {
+        if(userId === _id){
+            throw new CustomError("You can not unblock yourself", 500)
+        }
+        const userToUnblock = await User.findById(userId)
+        const loggedInUser = await User.findById(_id)
+
+        if(!userToUnblock || !loggedInUser) {
+            throw new CustomError("User not found!", 404)
+        }
+        if(!loggedInUser.blocklist.includes(userId)){
+            throw new CustomError("Not blocking this user!", 404)
+        }
+
+        loggedInUser.blocklist = loggedInUser.blocklist.filter(id=>id.toString()!= userId)
+
+        await loggedInUser.save()
+
+        res.status(200).json({message: "Successfully unblocked user!"})
+    } catch (error) {
+        
+    }
+}
+
+const getBlockedUsersController= async(req, res, next)=>{
+    const {userId} = req.params
+    try {
+        const user = await User.findById(userId).populate("blocklist", "username fullName profilePicture")
+        if(!user) {
+            throw new CustomError("User not found", 404)
+        }
+        const {blocklist, ...data} = user
+
+        res.status(200).json(blocklist)
+    }
+    catch(error) {
+        next(error)
+    }
+}
 
 
-module.exports = {getUserController, updateUserController,followUserController, unfollowUserController}
+
+module.exports = {getUserController, updateUserController,followUserController, unfollowUserController, blockUserController, unblockUserController, getBlockedUsersController}
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { getUserController, updateUserController, followUserController, unfollowUserController } = require("../controllers/userController")
+const { getUserController, updateUserController, followUserController, unfollowUserController, blockUserController, unblockUserController,getBlockedUsersController  } = require("../controllers/userController")
 const router = express.Router()
 
 router.get("/:userId", getUserController)
@@ -9,5 +9,13 @@ router.put("/update/:userId",updateUserController)
 router.post("/follow/:userId", followUserController)
 
 router.post("/unfollow/:userId", unfollowUserController)
+
+router.post("/block/:userId", blockUserController)
+
+router.post("/unblock/:userId", unblockUserController)
+
+router.get("/blocked/:userId", getBlockedUsersController)
+
+
 
 module.exports = router
